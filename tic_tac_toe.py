3caa866a95b1f5721a33e5d2a40ad9e22209774f
Fernando Pires dos Santos <fernandorj.21@hotmail.com>
2025-11-20 13:29:18 -0300

    Formato matriz! - Falta condições de vitoria!

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,15 +1,16 @@
 import pandas as pd
+import numpy as np
 
 class frame:
     def __init__(self):
-        self.tabuleiro = {i:0 for i in range(9)}
-        print('Bem-vindo ao Jogo da Velha created by Xibs')
+        self.tabuleiro = np.full((3, 3), ' ', dtype=str)
+        print('Bem-vindo ao Jogo da Velha created by Xibs.\n')
 
     def reiniciar_jogo(self):
-        self.tabuleiro = {i:0 for i in range(9)}
+        self.tabuleiro = np.full((3, 3), ' ', dtype=str)
     
     def locais_vazios(self):
-        self.empty_spc = [i for i,j in self.tabuleiro.items() if j==0]
+        self.empty_spc = [(i+1,j+1) for (i,j),valor in np.ndenumerate(self.tabuleiro) if valor == ' ']
         return self.empty_spc
     
     def retorna_tabuleiro(self):
@@ -19,11 +20,11 @@ class jogador:
     def __init__(self, tipo, Tabuleiro):
         self.player = tipo
         self.Tabuleiro = Tabuleiro
-        print(f'Bem-vindo player {self.player}.')
+        print(f'Bem-vindo jogador {self.player}.')
 
     def marcar(self, pos):
         tabuleiro_atm = self.Tabuleiro.retorna_tabuleiro()
-        self.Tabuleiro.tabuleiro[pos] = self.player
+        self.Tabuleiro.tabuleiro[int(pos[1])-1,int(pos[3])-1] = self.player
         print(tabuleiro_atm)
 
 
@@ -36,16 +37,17 @@ jogo = 0
 i = 1
 
 while jogo != 1:
-    jogada = int(input(f'Em qual das posições deseja jogar? {Tabuleiro.locais_vazios()}'))
-    if i % 2 == 0:
+
+    if i % 2 != 0:
+        print(f'\nPlayer X é sua vez de jogar!')
+        jogada = input(f'Onde deseja marcar? {Tabuleiro.locais_vazios()}')
         player_x.marcar(jogada)
     else:
+        print(f'\nPlayer O é sua vez de jogar!')
+        jogada = input(f'Onde deseja marcar? {Tabuleiro.locais_vazios()}')
         player_o.marcar(jogada)
 
     if Tabuleiro.locais_vazios() == []:
         jogo = 1
     else:
-        i += 1
-
-
-        
+        i += 1       
