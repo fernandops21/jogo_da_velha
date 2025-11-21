d90892be6d2860664552a4c53e0cf1f74e39352f
Fernando Pires dos Santos <fernandorj.21@hotmail.com>
2025-11-21 07:33:14 -0300

    correcao de bug de n poder jogar novamente no empate

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class jogo:
+class Jogo:
     def __init__(self):
         self.tabuleiro = np.array([str(i) for i in range(1,10)]).reshape(3, 3)
         print('Bem-vindo ao Jogo da Velha created by Xibs.\n')
@@ -27,40 +27,42 @@ class jogo:
         check_linha = any([(len(set(self.tabuleiro[i]))==1) for i in range(3)])
         check_coluna = any([(len(set(self.tabuleiro[:,i]))==1) for i in range(3)])
         check_diagonal = (len(set(diag_princ)) == 1 or len(set(diag_sec)) == 1)
-        if check_linha or check_coluna or check_diagonal:
+        check_spc = self.locais_vazios() == []
+
+        if check_linha or check_coluna or check_diagonal or check_spc:
            self.jogo = 1
         return self.jogo
     
-class jogador:
-    def __init__(self, tipo, Tabuleiro):
+class Jogador:
+    def __init__(self, tipo, Jogo):
         self.player = tipo
-        self.Tabuleiro = Tabuleiro
+        self.Jogo = Jogo
         print(f'Bem-vindo jogador {self.player}.')
 
     def marcar(self, pos):
-        mapa_tabuleiro = self.Tabuleiro.retorna_mapa()
+        mapa_tabuleiro = self.Jogo.retorna_mapa()
         coord_marcar = mapa_tabuleiro[pos]
-        self.Tabuleiro.tabuleiro[coord_marcar] = self.player
-        self.Tabuleiro.retorna_tabuleiro()
+        self.Jogo.tabuleiro[coord_marcar] = self.player
+        self.Jogo.retorna_tabuleiro()
 
-Tabuleiro = jogo()
-player_x = jogador('X', Tabuleiro)
-player_o = jogador('O', Tabuleiro)
+tabuleiro = Jogo()
+player_x = Jogador('X', tabuleiro)
+player_o = Jogador('O', tabuleiro)
 
-jogo = Tabuleiro.check_fim_de_jogo()
+jogo = tabuleiro.check_fim_de_jogo()
 i = 1
 
 while jogo != 1:
     invalido = True
 
     if i==1:
-        Tabuleiro.retorna_tabuleiro()
+        tabuleiro.retorna_tabuleiro()
 
     if i % 2 != 0:
         print(f'\nPlayer X é sua vez de jogar!')
         while invalido:
-            jogada = input(f'Onde deseja marcar? {Tabuleiro.locais_vazios()}: ')
-            if jogada in Tabuleiro.locais_vazios():
+            jogada = input(f'Onde deseja marcar? {tabuleiro.locais_vazios()}: ')
+            if jogada in tabuleiro.locais_vazios():
                 invalido = False
             else:
                 print('Jogada Inválida')
@@ -70,35 +72,35 @@ while jogo != 1:
     else:
         print(f'\nPlayer O é sua vez de jogar!')
         while invalido:
-            jogada = input(f'Onde deseja marcar? {Tabuleiro.locais_vazios()}: ')
-            if jogada in Tabuleiro.locais_vazios():
+            jogada = input(f'Onde deseja marcar? {tabuleiro.locais_vazios()}: ')
+            if jogada in tabuleiro.locais_vazios():
                 invalido = False
             else:
                 print('Jogada Inválida')
 
         player_o.marcar(jogada)
+    
+    jogo = tabuleiro.check_fim_de_jogo()
 
-    if Tabuleiro.locais_vazios() == []:
-        jogo = 1
-        print('\nFim de jogo. Empate!')
-    else:
-        jogo = Tabuleiro.check_fim_de_jogo()
-        if jogo == 1:
-            if i % 2 != 0:
-                print('\nFim de jogo. Parabéns jogador X!')
+    if jogo == 1:
+        if tabuleiro.locais_vazios() == []:
+            print('\nFim de jogo. Empate!')
+        elif i % 2 != 0:
+            print('\nFim de jogo. Parabéns jogador X!')
+        else:
+            print('\nFim de jogo. Parabéns jogador O!')
+
+        reset = '0'
+        while (reset != '1') and (reset != '2'):
+            reset = input('Deseja jogar novamente? 1 para sim 2 para não: ')
+            if reset == '1':
+                jogo = 0
+                i = 1
+                tabuleiro.reiniciar_jogo()
+                print('\nNovo Jogo Iniciado!')
+            elif reset == '2':
+                print('Fim de jogo')
             else:
-                print('\nFim de jogo. Parabéns jogador O!')
-
-            reset = '0'
-            while (reset != '1') and (reset != '2'):
-                reset = input('Deseja jogar novamente? 1 para sim 2 para não: ')
-                if reset == '1':
-                    jogo = 0
-                    Tabuleiro.reiniciar_jogo()
-                    print('\nNovo Jogo Iniciado!')
-                elif reset == '2':
-                    print('Fim de jogo')
-                else:
-                    print('Entrada inválida.')
-
+                print('Entrada inválida.')
+    else:
         i += 1
